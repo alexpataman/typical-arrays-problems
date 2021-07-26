451d85a41c15f5a2cb099800f466ac0736c8b9c2
alexpataman[mac] <alex.pataman@gmail.com>
2021-07-26 23:35:38 +0200

    init: task Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+exports.min = function min(array) {
+    return (
+        (array &&
+            array.reduce((acc, value) => (acc < value ? acc : value), 0)) ||
+        0
+    );
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return (
+        (array &&
+            array.reduce((acc, value) => (acc > value ? acc : value), 0)) ||
+        0
+    );
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    return (
+        (array &&
+            array.reduce((acc, value) => (acc += value), 0) / array.length) ||
+        0
+    );
+};
